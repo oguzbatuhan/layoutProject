85943779a9e6780e31f03a415da5d46846096ce8
Oğuz Batuhan Çözeli <oguzbatuhanczl@gmail.com>
2024-04-27 10:09:18 +0300

    Initial Commit

---
+++ layoutProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  layoutProject
+//
+//  Created by Oğuz Batuhan Çözeli on 27.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
